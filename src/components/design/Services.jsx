478619d2aaca59f394b0d352d2c0f36dbb726e2f
Matthew McGovern <mattmcg37@gmail.com>
2024-03-27 15:30:33 -0300

    bento boxes pt 2

--- src/components/design/Services.jsx
+++ src/components/design/Services.jsx
@@ -15,10 +15,10 @@ export const Gradient = () => {
   );
 };
 
-export const PhotChatMessage = () => {
+export const PhotoChatMessage = () => {
   return (
     <div className="absolute top-8 right-8 max-w-[17.5rem] py-6 px-8 bg-black rounded-t-xl rounded-bl-xl font-code text-base lg:top-16 lg:right-[8.75rem] lg:max-w-[17.5rem]">
-      Hey Brainwave, enhance this photo
+      Hey Alec, please help me with my taxes
       <ChatBubbleWing className="absolute left-full bottom-0" />
     </div>
   );
@@ -27,7 +27,7 @@ export const PhotChatMessage = () => {
 export const VideoChatMessage = () => {
   return (
     <div className="absolute top-8 left-[3.125rem] w-full max-w-[14rem] pt-2.5 pr-2.5 pb-7 pl-5 bg-n-6 rounded-t-xl rounded-br-xl font-code text-base md:max-w-[17.5rem]">
-      Video generated!
+      Anything you need!
       <div className="absolute left-5 -bottom-[1.125rem] flex items-center justify-center w-[2.25rem] h-[2.25rem] bg-color-1 rounded-[0.75rem]">
         <img
           src={brainwaveWhiteSymbol}
@@ -43,24 +43,6 @@ export const VideoChatMessage = () => {
         className="absolute right-full bottom-0 -scale-x-100"
         pathClassName="fill-n-6"
       />
-    </div>
-  );
-};
-
-export const VideoBar = () => {
-  return (
-    <div className="absolute left-0 bottom-0 w-full flex items-center p-6">
-      <img
-        src={play}
-        width={24}
-        height={24}
-        alt="Play"
-        className="object-contain mr-3"
-      />
-
-      <div className="flex-1 bg-[#D9D9D9]">
-        <div className="w-1/2 h-0.5 bg-color-1"></div>
-      </div>
     </div>
   );
 };
