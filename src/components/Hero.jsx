8595500d48c4dc69e26775d30d26dcf43c214914
Matthew McGovern <mattmcg37@gmail.com>
2024-04-04 19:34:14 -0300

    hero edits

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,8 @@
 import Button from './Button';
-import { curve, robot } from '../assets';
-import {  BottomLine, Gradient } from './design/Hero';
-import { heroIcons } from '../constants';
-import { ScrollParallax } from 'react-just-parallax';
+import { curve } from '../assets';
+import { Gradient } from './design/Hero';
+
 import { useRef } from 'react';
-import Generating from './Generating';
-import Notification from './Notification';
 import CompanyLogos from './CompanyLogos';
 
 
@@ -36,33 +33,18 @@ const Hero = () => {
           </Button>
         </div>
         <div className='relative max-w-[23rem] mx-auto md:max-w-5xl xl:mb-24'>
-          <div className='relative z-1 p-0.5 rounded-2xl bg-conic-gradient'>
+          <div className='relative z-1 p-0.5 rounded-2xl bg-iconic-gradient'>
             <div className='relative bg-n-8 rounded-[1rem]'>
               <div className='h-[1.4rem] bg-n-10 rounded-t-[0.9rem]' />
 
               <div className='aspect-[33/40] rounded-b-[0.9rem] overflow-hidden md:aspect-[688/490] lg:aspect-[1024/490]'>
 
 
-                <Generating className='absolute left-4 right-4 bottom-5 md:left-1/2 md:right-auto md:bottom-8 md:w-[31rem] md:-translate-x-1/2'/>
-                <ScrollParallax isAbsolutelyPositioned>
-                  <ul className="hidden absolute -left-[5.5rem] bottom-[7.5rem] px-1 py-1 bg-n-9/40 backdrop-blur border border-n-1/10 rounded-2xl xl:flex">
-                    {heroIcons.map((icon, index) => (
-                      <li className="p-5" key={index}>
-                        <img src={icon} width={24} height={25} alt={icon} />
-                      </li>
-                    ))}
-                  </ul>
-                </ScrollParallax>
 
-                <ScrollParallax isAbsolutelyPositioned>
-                    <Notification className='hidden absolute -right-[5.5rem] bottom-[11rem] w-[18rem] xl:flex' title="Code Generation" />
-                </ScrollParallax>
+
               </div>
             </div>
             <Gradient />
-          </div>
-          <div className='absolute -top-[54%] left-1/2 w-[234%] -translate-x-1/2 md:-top-[46%] md:w-[138%] lg:-top-[104%]'>
-
           </div>
 
 
@@ -70,7 +52,7 @@ const Hero = () => {
         </div>
         <CompanyLogos className='hidden relative z-10 mt-20 lg:block' />
     </div>
-    <BottomLine />
+
     </section>
   )
 }
