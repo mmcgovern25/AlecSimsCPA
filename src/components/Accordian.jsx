17b685938c6b29c90850d4bd85fc86efae2035c9
Matthew McGovern <mattmcg37@gmail.com>
2024-04-09 23:15:05 -0300

    starting faq

---
+++ src/components/Accordian.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Accordian = () => {
+  return (
+    <div className='py-2'>Accordian
+      <button className='flex justify-between'>
+        <span>this is title</span>
+        <span>+</span>
+      </button>
+    </div>
+  )
+}
+
+export default Accordian
--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -1,4 +1,4 @@
-import { smallSphere, stars } from "../assets";
+import Accordian from "./Accordian";
 import Heading from "./Heading";
 
 
@@ -14,29 +14,8 @@ const Pricing = () => {
           title="Pay once, use forever"
         />
 
-      <div className="container relative z-2">
-        <div className="relative justify-center mb-[6.5rem] lg:flex">
-          <img
-            src={smallSphere}
-            className="relative z-1"
-            width={255}
-            height={255}
-            alt="Sphere"
-          />
-          <div className="absolute top-1/2 left-1/2 w-[60rem] -translate-x-1/2 -translate-y-1/2 pointer-events-none">
-            <img
-              src={stars}
-              className="w-full"
-              width={950}
-              height={400}
-              alt="Stars"
-            />
-          </div>
-        </div>
+    <div className="p-4 bg-gray-200 rounded-lg"><Accordian /></div>
 
-
-
-      </div>
       </div>
     </section>
   )
