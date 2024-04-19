9018e595cc3685ade7a33fc5d537c1777b88ebca
Matthew McGovern <mattmcg37@gmail.com>
2024-04-18 21:07:04 -0300

    scroller done besides black background on some images

--- src/components/CompanyLogos.jsx
+++ src/components/CompanyLogos.jsx
@@ -1,5 +1,7 @@
 import './LogosJS';
 
+import Heading from "./Heading";
+
 import bryant from "../assets/bryant-resized.png";
 import pwc from "../assets/pwc5-removebg-preview-resized.png";
 import bluebird from "../assets/bbb-removebg-preview-resized.png";
@@ -9,9 +11,12 @@ import sas from "../assets/sas-removebg-preview-resized.png";
 
 
 
-const CompanyLogos = () => {
+const CompanyLogos = ({ className }) => {
   return (
 
+    <div className={` ${className}`}> {/* Adjust the margin top as needed */}
+      <Heading className="tagline mb-6 mt-20 text-center text-n-1/50"
+      tag="A BRIEF HISTORY OF MY PROFESSIONAL CAREER" />
 
 <div className="mt-20 scroller container" data-direction="right" data-speed="slow">
   <div className="scroller__inner">
@@ -21,6 +26,7 @@ const CompanyLogos = () => {
     <img src={cpa} alt="" />
     <img src={sas} alt="" />
   </div>
+</div>
 </div>
 
 
